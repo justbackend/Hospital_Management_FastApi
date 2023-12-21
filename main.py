44afe85e1d7dbe24719824c0781ffc646f0e6736
justbackend <izzatullaev2001abror@gmail.com>
2023-12-21 14:08:45 +0500

    cors

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from database import Base, engine
 from routes import users, temp_patients, admin, login, screen
 from routes.login import get_password_hash
-
+from starlette.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"])
 
 
 app.include_router(login.login_router)
