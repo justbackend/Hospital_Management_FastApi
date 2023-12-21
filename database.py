dead6458b3364d5d9f24f9518487f10824d88858
justbackend <izzatullaev2001abror@gmail.com>
2023-12-21 14:30:56 +0500

    database

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/clinic'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/clinic'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
